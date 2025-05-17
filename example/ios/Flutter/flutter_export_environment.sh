bf0a33f44a100ee5a5ee7e7c6968f5397a5b1ade
Arshman Afzal <arshman3@gmail.com>
2025-05-17 15:08:09 +0500

    Migrated to Null Safety

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=F:\Development\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Development\auto_direction\example"
+export "FLUTTER_ROOT=/Users/mehrozafzal/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mehrozafzal/Documents/CodeMiners/CM Plugins/auto_direction/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
